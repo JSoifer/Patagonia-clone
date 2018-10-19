dd23e0b6940363a44c1d11263b36e6313bbe500b
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-19 12:15:00 -0700

    create home-view, header, and header-bottom components.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './home-view/home-view.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderBottomComponent } from './header-bottom/header-bottom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    HeaderComponent,
+    HeaderBottomComponent
   ],
   imports: [
     BrowserModule
