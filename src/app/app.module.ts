c063d2a9a5991460a0ef1fc3bdda96bcd5388960
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-26 15:05:36 -0700

    add splash component and put header into root component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LowerHeroGridComponent } from './lower-hero-grid/lower-hero-grid.component';
 import { ShopComponent } from './shop/shop.component';
 import { routing } from './app.routing';
+import { SplashComponent } from './splash/splash.component';
 // import { InfoTile } from '././models/info-tile.model';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     LowerHeroGridComponent,
     ShopComponent,
+    SplashComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,17 +1,19 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
 import { ShopComponent } from './shop/shop.component';
-import { HeaderComponent } from './header/header.component';
+
 
 const appRoutes: Routes = [
   {
-    path: 'shop',
-    component: ShopComponent
+    path: '',
+    component: SplashComponent
   },
   {
-    path: 'header',
-    component: HeaderComponent
+    path: 'shop',
+    component: ShopComponent
   }
+
 ];
 
 
