83fd0aea920ac7d94915e3bcefee875fff42b827
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-28 16:34:03 -0700

    add model for item and temporary mock data base.

---
+++ src/app/mock-items.ts
@@ -0,0 +1,5 @@
+import { Item } from './models/item.model';
+
+export const ITEMS: Item[] = [
+  new Item("Long-Sleeved Island Hopper Shirt", "./assets/shirt1.jpg", "Celery Green - Used", 50, "Medium", "Men's", "itemNumber", "Celery Green", true)
+];
---
+++ src/app/models/item.model.ts
@@ -0,0 +1,12 @@
+export class Item {
+  constructor(
+    public title: string,
+    public imageURL: string,
+    public description: string,
+    public price: number,
+    public size: string,
+    public category: string,
+    public id: string,
+    public color: string,
+    public inStock: boolean,) {}
+}
