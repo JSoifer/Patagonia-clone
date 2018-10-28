bfabe621d5ce38fa6d6fcb89dd8cbd1bb76e92d7
Julianne Soifer <juliannesoifer@gmail.com>
2018-10-28 12:34:31 -0700

    display lower-hero-grid to root component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
 
 
 masterInfoTiles: InfoTile[] = [
-  new InfoTile('./assets/trade-it.jpg', './assets/blockTradeItIn.png', 'Trade in your used Patagonia gear at any Patagonia retail location and receive credit to put toward another new or used garment.', '#'),
+  new InfoTile('./assets/trade-it.jpg', './assets/block-TradeItIn.png', 'Trade in your used Patagonia gear at any Patagonia retail location and receive credit to put toward another new or used garment.', '#'),
   new InfoTile('./assets/shop-used.jpeg', './assets/block-BuyItUsed.png', 'Save and reuse by shopping used Patagonia gear—quality stuff that will last, or can be traded back to us when you are done.','#'),
-  new InfoTile('.assets/repair.jpg', './assets/block-RepairIt.png', 'One of the most responsible things we can do as a company is to make high-quality stuff that lasts for years and can be repaired, so you don’t have to buy more of it.','#')
+  new InfoTile('./assets/repair.jpg', './assets/block-RepairIt.png', 'One of the most responsible things we can do as a company is to make high-quality stuff that lasts for years and can be repaired, so you don’t have to buy more of it.','#')
 ];
 }
